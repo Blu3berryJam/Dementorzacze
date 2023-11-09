13e85ec9b37a2cf2347359b0e25f6afd6031ef3a
Konrad Drozd <konraddrozd02@gmail.com>
2023-11-09 19:13:57 +0100

    code quality update

--- src/scraper/main.py
+++ src/scraper/main.py
@@ -15,15 +15,17 @@ def get_subcategories(category):
     subs = category.find('ul', {"class": "level2"}).find_all('li',recursive=False)
     subcategories = []
     for s in subs:
-        subcategories.append((s.find('span').get_text(),s.find('a')['href']))
+        subcategories.append({"name":s.find('span').get_text(),"link":s.find('a')['href']})
     return subcategories
 
 def get_products(cat_name,subcategory):
-    page = requests.get(url+str(subcategory[1]))
+    page = requests.get(url+str(subcategory['link']))
     soup = BeautifulSoup(page.content, 'html.parser')
     product_divs = soup.find_all('div',{"class": "product-inner-wrap"})
-    if len(product_divs) == 0:
+
+    if len(product_divs) == 0: #if no products in subcategory
         return []
+
     products = []
     for product in product_divs:
         link = product.find('a',{"class": "prodimage f-row"})['href'] #get product page link
@@ -35,25 +37,28 @@ def get_products(cat_name,subcategory):
         with open(img_path,'wb') as handler:
             handler.write(img_data) #save product miniature
 
-        products.append({"name":name,"brand":brand,"description":desc,"power": power,"dimensions":dimensions,"price":price,"category":cat_name,"subcategory":subcategory[0]}) #add product info to products list
+        products.append({"name":name,"brand":brand,"description":desc,"power": power,"dimensions":dimensions,"price":price,"category":cat_name,"subcategory":subcategory['name']}) #add product info to products list
     return products
 
 def get_product_info(link):
     page = requests.get(url + link)
     soup = BeautifulSoup(page.content, 'html.parser')
     name = soup.find('h1',{"class": "name"}).get_text().strip() #get product name
-    name = re.sub('[^A-Za-z0-9 +\-/.,_:()*@#!$%^&]+', '', name)
+    name = re.sub('[^A-Za-z0-9 +\-/.,_:()*@#!$%^&]+', '', name) #remove weird characters
     decription = power = dimensions = ""
     decription_div = soup.find('div',{"class":"resetcss"}) #get product description
     table_elements = soup.find('div',{"class":"resetcss"}).find_all('li') #get product desription list items
+
     for element in table_elements:
         if atribute1 in element.get_text(): #check if list item is vacuum power atribute
             power = element.get_text()
         if atribute2 in element.get_text(): #check if list item is vacuum dimensions atribute
             dimensions = element.get_text()
+
     if decription_div is not None:
         decription = decription_div.get_text()
-    price = soup.find('em',{"class":"main-price"}).get_text()
+
+    price = soup.find('em',{"class":"main-price"}).get_text() #get brand price
     brand = soup.find('a',{"class":"brand"}) #get brand name <a> element
     if brand is not None:#if brand is not none get brand name
         brand = brand.get_text().strip()
@@ -74,6 +79,7 @@ if __name__ == '__main__':
         os.mkdir(low_res_folder, 0o666)
     if not os.path.exists(high_res_folder):
         os.mkdir(high_res_folder, 0o666)
+
     names = []
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -84,6 +90,7 @@ if __name__ == '__main__':
     for c in list_categories:
         names.append(c.find('span').get_text())
         subcategories.append(get_subcategories(c))
+
     products = []
     for i in range(len(names)):
         for subcategory in subcategories[i]:
