dfe70cee20ed72b4566ed2ee26d593f200eb126e
Konrad Drozd <konraddrozd02@gmail.com>
2023-11-09 19:08:09 +0100

    completed the basic scraper
    added image scarpping

--- src/scraper/main.py
+++ src/scraper/main.py
@@ -1,9 +1,15 @@
+import os
+import re
+
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 url = "https://eodkurzacz.pl/"
 atribute1 = "Moc odkurzacza"
 atribute2 = "Wymiary\xa0odkurzacza"
+low_res_folder = "../../images_low"
+high_res_folder = "../../images_high"
 
 def get_subcategories(category):
     subs = category.find('ul', {"class": "level2"}).find_all('li',recursive=False)
@@ -20,30 +26,54 @@ def get_products(cat_name,subcategory):
         return []
     products = []
     for product in product_divs:
-        link = product.find('a',{"class": "prodimage f-row"})['href']
-        name,desc,price,power,dimensions = get_product_info(link)
-        products.append({"name":name,"description":desc,"power": power,"dimensions":dimensions,"price":price,"category":cat_name,"subcategory":subcategory[0]})
+        link = product.find('a',{"class": "prodimage f-row"})['href'] #get product page link
+        name,desc,price,power,dimensions,brand = get_product_info(link) #get product info from product page
+        print(brand)
+        low_res_img_link  = product.find('img')['data-src'] #get product miniature link
+        img_data = requests.get(url + low_res_img_link).content #get product miniature
+        img_path = low_res_folder + "/" + name.replace("/","") + ".jpg" #product miniature name and path
+        with open(img_path,'wb') as handler:
+            handler.write(img_data) #save product miniature
+
+        products.append({"name":name,"brand":brand,"description":desc,"power": power,"dimensions":dimensions,"price":price,"category":cat_name,"subcategory":subcategory[0]}) #add product info to products list
     return products
 
 def get_product_info(link):
     page = requests.get(url + link)
     soup = BeautifulSoup(page.content, 'html.parser')
-    name = soup.find('h1',{"class": "name"}).get_text()
+    name = soup.find('h1',{"class": "name"}).get_text().strip() #get product name
+    name = re.sub('[^A-Za-z0-9 +\-/.,_:()*@#!$%^&]+', '', name)
     decription = power = dimensions = ""
-    decription_div = soup.find('div',{"class":"resetcss"})
-    table_elements = soup.find('div',{"class":"resetcss"}).find_all('li')
+    decription_div = soup.find('div',{"class":"resetcss"}) #get product description
+    table_elements = soup.find('div',{"class":"resetcss"}).find_all('li') #get product desription list items
     for element in table_elements:
-        if atribute1 in element.get_text():
+        if atribute1 in element.get_text(): #check if list item is vacuum power atribute
             power = element.get_text()
-        if atribute2 in element.get_text():
+        if atribute2 in element.get_text(): #check if list item is vacuum dimensions atribute
             dimensions = element.get_text()
     if decription_div is not None:
         decription = decription_div.get_text()
     price = soup.find('em',{"class":"main-price"}).get_text()
-    return name,decription,price,power,dimensions
+    brand = soup.find('a',{"class":"brand"}) #get brand name <a> element
+    if brand is not None:#if brand is not none get brand name
+        brand = brand.get_text().strip()
+    else: #else set brand to unknown
+        brand = "-"
+
+    high_res_link = soup.find('div',{"class": "mainimg productdetailsimgsize row"}).find('img')['src']  # get product image link
+    img_data = requests.get(url + high_res_link).content  # get product image
+    img_path = high_res_folder + "/" + name.replace("/", "") + ".jpg"  # product image name and path
+    with open(img_path, 'wb') as handler:
+        handler.write(img_data)  # save product image
+
+    return name,decription,price,power,dimensions,brand
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    if not os.path.exists(low_res_folder):
+        os.mkdir(low_res_folder, 0o666)
+    if not os.path.exists(high_res_folder):
+        os.mkdir(high_res_folder, 0o666)
     names = []
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
