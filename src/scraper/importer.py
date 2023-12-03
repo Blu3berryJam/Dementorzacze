05882f9383d606732d76748b2162bb58c9af6818
Michał Jaroszewicz <michjar.2002@gmail.com>
2023-12-03 23:22:20 +0100

    data import

--- src/scraper/importer.py
+++ src/scraper/importer.py
@@ -136,8 +136,8 @@ del prod_schema["product"]["associations"]["combinations"]
 
 brand_schema = prestashop.get("manufacturers", options={"schema": "blank"})
 
-remove_categories()
-remove_products()
-remove_brands()
+#remove_categories()
+#remove_products()
+#remove_brands()
 add_categories()
 add_products()
