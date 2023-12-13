842a5df1eb4300875b47a2ea8c38b9c8eca0149b
fxn0 <filipoda5@wp.pl>
2023-12-13 20:18:58 +0100

    delete everything

--- ssl/enableSSL.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-bash cert_key_gen.sh
-docker exec prestashop mkdir /etc/ssl/dementorzacze
-docker cp keys prestashop:/etc/ssl/dementorzacze/keys
-docker cp default-ssl.conf prestashop:/etc/apache2/sites-available/default-ssl.conf
-docker exec prestashop ln -s /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled/default-ssl.conf
-docker exec prestashop a2enmod ssl
-docker exec prestashop service apache2 restart
